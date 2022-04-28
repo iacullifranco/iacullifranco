065b50affb269a7428d7943668aad362c07bd41a
iacullifranco <104537572+iacullifranco@users.noreply.github.com>
2022-04-27 21:35:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clase_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
